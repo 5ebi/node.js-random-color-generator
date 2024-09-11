e6c346b9f946a3091f07ded0143caf8f301e181e
Sebastian <sebi.speiser@gmx.net>
2024-09-11 20:14:58 +0200

    hue and sat added

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # Node.js Random Color Generator Fall 2024
 
-- [ ] abcde
+Display random colors on the command line
+
+## Random colors
+
+## Random colors with input
+
+-hue (eg. green or red or blue)
+-luminosity (eg- light or dark)
--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 import chalk from 'chalk';
 import randomColor from 'randomcolor';
 
-const color = randomColor();
+const hue = process.argv[2];
+const luminosity = process.argv[3];
+
+const color = randomColor({
+  luminosity: luminosity,
+  hue: hue,
+});
 const square = [
   '###############################',
   '###############################',
